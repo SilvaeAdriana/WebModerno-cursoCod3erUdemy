c1f4dd5ced15f1723476f8a5375f928a77f67214
A Driana <75712203+SilvaeAdriana@users.noreply.github.com>
2023-02-05 17:26:52 -0300

    versões com anotações

--- fundamentos/usandoLetEmLoop2.js
+++ fundamentos/usandoLetEmLoop2.js
@@ -6,6 +6,8 @@ for (let i = 0; i < 10; i++) {
     })
 }
 
+//imprime os numeros em colchetes
+//como i tem escopo de bloco por ser let, ele tem seu valor recuperado
 funcs[2]()
 funcs[6]()
 funcs[8]()
--- fundamentos/usandoVarEmLoop2.js
+++ fundamentos/usandoVarEmLoop2.js
@@ -1,10 +1,12 @@
 const funcs = []
 
 for (var i = 0; i < 10; i++) {
-    funcs.push(function() {
+    funcs.push(function() {//function é uma funcao anônima
         console.log(i)
     })
 }
+
+//o resultado é 10 para ambos, por var n tem escopo de bloco
 
 funcs[2]()
 funcs[8]()
